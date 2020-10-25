3c48fd135d4f6bf180105c1b29c93046fbd91bcb
Abhivineet <abhivineetv@gmail.com>
2020-10-25 00:58:39 -0700

    Updated minmax, divide error

--- test.py
+++ test.py
@@ -16,15 +16,15 @@ def main():
 
     x_train, y_train, x_val, y_val, x_test, y_test = data_processing()
 
-    print('x_train shape = ', x_train.shape)
-    print('y_train shape = ', y_train.shape)
-
-    tuner_without_scaling_obj = HyperparameterTuner()
-    tuner_without_scaling_obj.tuning_without_scaling(distance_funcs, x_train, y_train, x_val, y_val)
-
-    print("**Without Scaling**")
-    print("k =", tuner_without_scaling_obj.best_k)
-    print("distance function =", tuner_without_scaling_obj.best_distance_function)
+    # print('x_train shape = ', x_train.shape)
+    # # print('y_train shape = ', y_train.shape)
+    # #
+    # # tuner_without_scaling_obj = HyperparameterTuner()
+    # # tuner_without_scaling_obj.tuning_without_scaling(distance_funcs, x_train, y_train, x_val, y_val)
+    # #
+    # # print("**Without Scaling**")
+    # # print("k =", tuner_without_scaling_obj.best_k)
+    # # print("distance function =", tuner_without_scaling_obj.best_distance_function)
 
     tuner_with_scaling_obj = HyperparameterTuner()
     tuner_with_scaling_obj.tuning_with_scaling(distance_funcs, scaling_classes, x_train, y_train, x_val, y_val)
--- utils.py
+++ utils.py
@@ -179,27 +179,6 @@ class HyperparameterTuner:
                         best_model = model
                         best_scaler = scale
                         last_best_f1 = f1
-                    # if f1 == last_best_f1:
-                    #     if best_scaler!=scale:
-                    #         if scale == "min_max_scale":
-                    #             best_k = k
-                    #             best_distance_function = func
-                    #             best_model = model
-                    #             best_scaler = scale
-                    #             last_best_f1 = f1
-                    #     elif best_distance_function!=func:
-                    #         if func=="euclidean":
-                    #             best_k = k
-                    #             best_distance_function = func
-                    #             best_model = model
-                    #             best_scaler = scale
-                    #             last_best_f1 = f1
-                    #         if func == "Minkowski" and best_distance_function=="cosine_dist":
-                    #             best_k = k
-                    #             best_distance_function = func
-                    #             best_model = model
-                    #             best_scaler = scale
-                    #             last_best_f1 = f1
 
         self.best_k = best_k
         self.best_distance_function = best_distance_function
@@ -258,7 +237,11 @@ class MinMaxScaler:
         """
         features = np.asarray(features, dtype='float')
         for i in range(len(features[0])):
-            features[:,i] = (features[:,i] - features[:,i].min())/(features[:,i].max() - features[:,i].min())
+            den = features[:,i].max() - features[:,i].min()
+            if den ==0:
+                features[:,i] = np.zeros(np.shape(features[:,i]))
+            else:
+                features[:,i] = (features[:,i] - features[:,i].min())/(den)
         return features
 
         # raise NotImplementedError
